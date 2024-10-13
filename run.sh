546a2fd2e36b2cbc997b215f1297088ab1180e47
chupino <mauricioescalante903@gmail.com>
2024-10-13 11:15:25 -0500

    a

--- run.sh
+++ run.sh
@@ -1,8 +1,23 @@
 git clone https://github.com/dockersamples/node-bulletin-board.git nodesql
+if [ $? -ne 0 ]; then
+    echo "Error al clonar nodesql"
+    exit 1
+fi
+
+sleep 3
+
 git clone https://github.com/chupino/solucionfinaIIab10.git solucion
+if [ $? -ne 0 ]; then
+    echo "Error al clonar solucion"
+    exit 1
+fi
+
+sleep 3
+
 cd nodesql
 git checkout v5
-mv ../solucion/ .
+
+cp -rf ../solucion/. .
 
 docker-compose up --build -d
 

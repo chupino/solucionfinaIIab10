6ec527bd865411ed81cdab117bf35c1815590213
chupino <mauricioescalante903@gmail.com>
2024-10-13 10:42:20 -0500

    a

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/dockersamples/node-bulletin-board.git nodesql
-git clone   solucion
+git clone https://github.com/chupino/solucionfinaIIab10.git solucion
 cd nodesql
 git checkout v5
 mv ../solucion/ .
